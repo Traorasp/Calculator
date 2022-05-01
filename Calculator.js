0378f883f231d2890de0b28c4e1146feeaef9e2f
Traorasp <miguelsolanopaz@outlook.com>
2022-05-01 01:46:01 -0400

    Changed operate function to work with buttons, created changeDisplay to change calculator changeDisplay when buttons arre pressed.

--- Calculator.js
+++ Calculator.js
@@ -1,4 +1,12 @@
-console.log(operate(5,"add",3));
+const calcDisplay = document.querySelector('#calcDisplay');
+
+
+const allButtons = Array.from(document.querySelectorAll('button'));
+allButtons.map(button => button.addEventListener('click', changeDisplay))
+
+function changeDisplay(button){
+    calcDisplay.textContent += button.target.textContent;
+}
 
 //Returns the sum of two numbers 
 function add(num1, num2){
@@ -26,17 +34,18 @@ function mod(num1, num2){
     return num1 % num2;
 }
 
+//Calls operator given on the two numbers given
 function operate(num1, operator, num2){
     switch (operator) {
-        case "add":
+        case "+":
             return add(num1, num2);
-        case "subtract":
+        case "-":
             return subtract(num1, num2);
-        case "multiply": 
+        case "*": 
             return multiply(num1, num2);
-        case "divide": 
+        case "/": 
             return divide(num1, num2);
-        case "mod":
+        case "%":
             return mod(num1, num2);
     }
 }
