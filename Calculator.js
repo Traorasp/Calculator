362584c125d93b0e577ddcceb2a4004632bf9e30
Traorasp <miguelsolanopaz@outlook.com>
2022-05-01 01:18:49 -0400

    Added basic operation functions to Calculator.js and checked they worked

---
+++ Calculator.js
@@ -0,0 +1,25 @@
+//Returns the sum of two numbers 
+function add(num1, num2){
+    return num1 + num2;
+}
+
+//Returns the value of one number being subtracted by another
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+
+//Returns the value of two numbers being multiplied
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+//Returns the value of on number being divided by another
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+//Returns the value of one value being modded by another
+function mod(num1, num2){
+    return num1 % num2;
+}
